4e17c237612d3dcf718d6e6b812ed8c09497b4e1
Gabriel Fuentes <gfuentes983@west-mec.org>
2019-04-01 15:19:53 -0700

    Created nodes and attributes nodes.
    Used nodes and attributes nodes as well as new properties and methods to create and change HTML.

---
+++ bc_switch.js
@@ -0,0 +1,91 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 12
+   Tutorial Case
+
+   Author: Gabriel Fuentes
+   Date: 3.28.19  
+
+   Filename: bc_switch.js
+   
+   setupStyles()
+   Function to set up the style sheet switcher and insert
+   form buttons to allow the user to switch between web
+   view and page view
+   
+*/
+window.addEventListener('load', setupStyles);
+
+function setupStyles() {
+   // body...
+   //  Create a link element for the page view styles
+   var pageStyle = document.createElement("link");
+   pageStyle.setAttribute("href", "bc_page.css");
+   pageStyle.setAttribute("rel", "stylesheet");
+   pageStyle.setAttribute("disabled", "disabled");
+
+
+   // Append the link element to the doucment head
+   document.head.appendChild(pageStyle);
+   pageStyle.disabled = true;
+
+   // Insert buttons for the style switcher
+   var buttonDIV = document.createElement("div");
+   buttonDIV.setAttribute("id", "styleButtons");
+
+   var webButton = document.createElement("input");
+   webButton.setAttribute('type', "button");
+   webButton.setAttribute('value', "Web View");
+
+   var pageButton = document.createElement("input");
+   pageButton.setAttribute('type', "button");
+   pageButton.setAttribute('value', "Page View");
+   
+   buttonDIV.appendChild(webButton);
+   buttonDIV.appendChild(pageButton);
+
+   document.body.insertBefore(buttonDIV, document.body.firstChild);
+
+   // Append an embedded stylesheet to the document head
+   var buttonSyles = document.createElement("style");
+   document.head.appendChild(buttonSyles);
+
+   // Add stiles rules to the embedded style sheet
+   doucment.styleSheets[doucment.styleSheets.length-1].insertRule(
+      "div#styleButtons {  \
+         position: fixed; \
+      }", 0);
+
+   doucment.styleSheets[doucment.styleSheets.length-1].insertRule(
+      "div#styleButtons input {  \
+         background-color: rgba(68, 94, 186, 0.6); \
+         border: 3px solid rgba(0, 24, 123, 0.6); \
+         border-radius: 50%; \
+         cursor: pointer; \
+         color: white; \
+         display: inline-block \
+         font-size: 1.2em; \
+         height: 60px; \
+         margin: 5px 10px; \
+         width: 100px; \
+      }", 1);
+
+   doucment.styleSheets[doucment.styleSheets.length-1].insertRule(
+      "@media print { \
+            div#styleButtons { \
+               display: none; \
+            } \
+         }", 2);
+
+   // Turn the page view style off and on 
+   webButton.onlick = function() {
+      pageStyle.disabled = true;
+   }
+
+   pageButton.onclick = function() {
+      // body...
+      pageStyle.disabled = false;
+   }
+}
